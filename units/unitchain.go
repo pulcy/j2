97252e1d8ba749b6e1d26ce5260f274bcef9207c
Ewout Prangsma <ewout@prangsma.net>
2016-02-29 13:21:53 +0100

    Fixed unitchain linking in case of a chain of 1 unit

--- units/unitchain.go
+++ units/unitchain.go
@@ -19,20 +19,22 @@ type UnitChain []*Unit
 // Link adds Require & After attributes on the units in the chain
 // in order to enfore the correct chain.
 func (chain UnitChain) Link() {
-	for i, u := range chain {
-		if i+1 < len(chain) {
-			// Forward requirement
-			u.ExecOptions.Require(chain[i+1].FullName)
-		}
-		if i > 0 {
-			// Backward requirement and after
-			u.ExecOptions.Require(chain[i-1].FullName)
-			u.ExecOptions.After(chain[i-1].FullName)
-			// machine-of previous unit in chain
-			u.FleetOptions.MachineOf(chain[i-1].FullName)
-		} else if len(chain) > 0 {
-			// require last unit in chain
-			u.ExecOptions.Require(chain[len(chain)-1].FullName)
+	if len(chain) > 1 {
+		for i, u := range chain {
+			if i+1 < len(chain) {
+				// Forward requirement
+				u.ExecOptions.Require(chain[i+1].FullName)
+			}
+			if i > 0 {
+				// Backward requirement and after
+				u.ExecOptions.Require(chain[i-1].FullName)
+				u.ExecOptions.After(chain[i-1].FullName)
+				// machine-of previous unit in chain
+				u.FleetOptions.MachineOf(chain[i-1].FullName)
+			} else if len(chain) > 0 {
+				// require last unit in chain
+				u.ExecOptions.Require(chain[len(chain)-1].FullName)
+			}
 		}
 	}
 }
